4cc7d4431b500b16551d4fc49bbc4ba34e34ec78
thomas <thomas.perret43@gmail.com>
2018-03-09 15:08:48 +0100

    yes

--- faker.php
+++ faker.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'vendor/autoload.php';
-require_once 'src/domain/Pokemon.php';
+require_once 'src/domain/Pokemon.php'; // pour faker1
 // alternatively, use another PSR-0 compliant autoloader (like the Symfony2 ClassLoader for instance);
 // use the factory to create a Faker\Generator instance
-$faker = Faker\Factory::create('fr_FR');
+/*$faker = Faker\Factory::create('fr_FR');
 
 // generate data by accessing properties
 
@@ -16,9 +16,12 @@ echo $faker->text, "</br>";
 // Dolores sit sint laboriosam dolorem culpa et autem. Beatae nam sunt fugit
 // et sit et mollitia sed.
 // Fuga deserunt tempora facere magni omnis. Omnis quia temporibus laudantium
-// sit minima sint.
+// sit minima sint.*/
 
 
 $faker1 = new Faker\Generator();
 $faker1->addProvider(new DTA\Faker\Provider\Pokemon($faker1));
-echo $faker1->Pokemon;
+
+for ($i=0; $i < 6; $i++) {
+    echo $faker1->Pokemon, "</br>";
+}
