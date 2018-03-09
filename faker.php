81e0b60248c5c33de359bb56aba2d43d44673bfd
thomas <thomas.perret43@gmail.com>
2018-03-09 15:11:22 +0100

    yes

--- faker.php
+++ faker.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 require_once 'src/domain/Pokemon.php'; // pour faker1
 // alternatively, use another PSR-0 compliant autoloader (like the Symfony2 ClassLoader for instance);
 // use the factory to create a Faker\Generator instance
-/*$faker = Faker\Factory::create('fr_FR');
+$faker = Faker\Factory::create('fr_FR');
 
 // generate data by accessing properties
 
@@ -12,11 +12,11 @@ echo $faker->name,"</br>";
 echo $faker->address, "</br>";
 // "426 Jordy Lodge
 // Cartwrightshire, SC 88120-6700"
-echo $faker->text, "</br>";
+echo $faker->text, "</br></br>";
 // Dolores sit sint laboriosam dolorem culpa et autem. Beatae nam sunt fugit
 // et sit et mollitia sed.
 // Fuga deserunt tempora facere magni omnis. Omnis quia temporibus laudantium
-// sit minima sint.*/
+// sit minima sint.
 
 
 $faker1 = new Faker\Generator();
